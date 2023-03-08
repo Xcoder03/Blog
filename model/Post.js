51a3e2214debe8787bc4fb7de9a020c3af4c3d1a
Xcoder <chindaebube03@gmail.com>
2023-03-09 00:22:29 +0100

    finished the post model

---
+++ model/Post.js
@@ -0,0 +1,60 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+   title:{
+    type:String,
+    required:[true,"Title of the post is required"],
+    trim:true
+   },
+   description:{
+    type:String,
+    required:[true,"Description of the post is required"],
+   },
+   category:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Category",
+    required:[true,"Category of the post is required"],
+   },
+   numViews:[
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },
+   ],
+   likes:[
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },
+   ],
+   dislikes:[
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },
+   ],
+  comments:[
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Comment",
+    }
+   
+  ],
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:[true,"Please author is required"],
+  },
+  photo:{
+    type:String
+  },
+},{
+    timestamps:true,
+    toJSON:{virtuals:true}
+}
+);
+
+const Post = mongoose.model("Post",postSchema);
+
+export default Post;
