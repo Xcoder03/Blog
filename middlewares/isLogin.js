e02b63c8180910148bc1f3e0588de2b83f8dc54d
Xcoder <chindaebube03@gmail.com>
2023-03-21 17:58:34 +0100

    created a middleware function to check if user is logged in

---
+++ middlewares/isLogin.js
@@ -0,0 +1,22 @@
+import { obtainToken } from "../utils/obtainToken";
+import { verifyToken } from "../utils/verifyToken";
+
+export const isLogin = (req, res,next)=>{
+    //get token header
+   const token = obtainTokenFromHeader(req);
+   //verify
+ 
+   const userDeCoded  = verifyToken(token);
+ 
+   req.userAuth = userDeCoded.id;
+ 
+   if(!userDeCoded){
+     return res.json({
+         status:"error",
+         message:"Kindly login in, because it seems the token is either expired or invalid"
+     })
+   }else{
+     next()
+   }
+    
+ }
