d8fb58b587951c3059a3417fd44edbf8b165a0ae
Xcoder <chindaebube03@gmail.com>
2023-03-31 16:53:40 +0100

    created middleware for validating user input.

---
+++ middlewares/userValidation.js
@@ -0,0 +1,18 @@
+import joi from   'joi'
+import User from '../model/User'
+
+
+export const validateUser = async(req, res, next) =>{
+    const Schema = joi.object({
+        firstname: joi.string().min(2).required(),
+        lastname: joi.string().min(2).required(),
+        email: joi.string().email().required(),
+        password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{5,15}$'))
+    })
+
+    const validateResult = Schema.validate(req.body);
+    if(!validateResult.error){
+        return res.status(400).json(validateResult.error.message)
+    }
+    next()
+}
